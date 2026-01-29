212a19d56806adf51efcb9a9395e35c514406915
Annie Tadvick <153228151+anddude@users.noreply.github.com>
2026-01-29 17:52:08 -0500

    Restructure VCS (#9)
    * Fix/gitignore and lockfile (#2)
    
    * Add CI pipeline with GitHub Actions
    
    * Include package-lock.json by running npm install locally for CI
    
    * Remove node_modules from version control
    
    * Fix .gitignore to properly ignore node_modules
    
    ---------
    
    Co-authored-by: Annie Tadvick <annie.tadvick@gmail.com>
    
    * Annie (#5)
    
    * Add CI pipeline with GitHub Actions
    
    * Include package-lock.json by running npm install locally for CI
    
    * Remove node_modules from version control
    
    * Fix .gitignore to properly ignore node_modules
    
    * Test ci / cd
    
    * Fix ci
    
    * Fix CI to run on PRs only
    
    ---------
    
    Co-authored-by: Annie Tadvick <annie.tadvick@gmail.com>
    
    * Fixes MIL-111: verify auto-resolution (#6)
    
    * Add CI pipeline with GitHub Actions
    
    * Include package-lock.json by running npm install locally for CI
    
    * Remove node_modules from version control
    
    * Fix .gitignore to properly ignore node_modules
    
    * Test ci / cd
    
    * Fix ci
    
    * Fix CI to run on PRs only
    
    * Part of MIL-111: test Linear GitHub integration
    
    * Closes  MIL-111: test Linear GitHub integration
    
    ---------
    
    Co-authored-by: Annie Tadvick <annie.tadvick@gmail.com>
    
    * fixes MIL-25:put some testing in (#7)
    
    * part of MIL-25: testing that the linear flag works
    
    * fixes MIL-25:put some testing in
    
    ---------
    
    Co-authored-by: Amen Stoney <amenstoney@gmail.comgit config --global user.name Amen>
    
    * part of MIL-53 Connect to DOM and render page (#8)
    
    ---------
    
    Co-authored-by: Annie Tadvick <annie.tadvick@gmail.com>
    Co-authored-by: Amen Stoney <112218549+tehutiboy20@users.noreply.github.com>
    Co-authored-by: Amen Stoney <amenstoney@gmail.comgit config --global user.name Amen>
    Co-authored-by: xglover410 <xglover410@gmail.com>

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,3 +4,13 @@
       - logic for flipping between sections
       - button logic for each component
 ================================ */
+import React from "react"
+
+
+export default function App() {
+  return (
+    <>
+    <h1>Testing Testing 123</h1>
+    </>
+  )
+}
