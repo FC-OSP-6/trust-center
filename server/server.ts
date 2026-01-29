1c3b254252ff69ba0d8dbf13f8048543714abab0
Amen Stoney <112218549+tehutiboy20@users.noreply.github.com>
2026-01-29 12:02:38 -0500

    fixes MIL-25:put some testing in (#7)
    * part of MIL-25: testing that the linear flag works
    
    * fixes MIL-25:put some testing in
    
    ---------
    
    Co-authored-by: Amen Stoney <amenstoney@gmail.comgit config --global user.name Amen>

--- server/server.ts
+++ server/server.ts
@@ -4,3 +4,17 @@
   - sets up express middleware
   - mounts /api/health and /graphql
 ================================ */
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log('Server ready');
+  
+});
