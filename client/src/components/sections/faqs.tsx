2838bb470d0b99ff9afd841963c378454992632d
morrisxelijah <morrisxelijah@gmail.com>
2026-02-10 10:33:00 -0500

    removing generated stencil files from tracking (#28)
    * Part of mil-137: EP0-T5a Lock consumption strategy
    
    * fixes MIL-143: add stretch sections and seperates to client only and server only
    
    * Part of mil-137: Add .stencil/ to git ignore list. Remove .vscode folder.
    
    * Part of mil-137: Add tsconfig for stencil compiler to auto-generate types in dist
    
    * Closes mil-137: stencil config is working. Add separate package.json in stencil.
    
    * closes MIL-128: sripts are in place, stencil runs, orchestrator exits cleanly
    
    * Part of mil-139: Create footer web component. Use as reference for further components.
    
    * chore: remove generated Stencil build output
    
    * Part of mil-139Update gitignore to account for stencil generated files.
    
    * fixes MIL-128: adds stencil build to main dev script
    
    * PR save
    
    * fixes MIL-128: updates cleanexit script to sleep before the intentional error (make the failure more apparent)
    
    * Add Aon foundations color scheme
    
    * Start of tokens.css file for stencil styling
    
    * closes MIL-130: /api/health returns JSON and GraphiQL for yoga shows up in the browser
    
    * closes MIL-132: seperates server creation and start for testing  +  proxies health check  +  mounts GraphQL
    
    * merge Mila into Elijah for backend setup, yoga server and schema (#17)
    
    Co-authored-by: milapaprika <mila.henson@protonmail.com>
    
    * closes MIL-135: graphql is created and mounts correctly, context shapes are defined, uuids are generated, post/ to graphql is stable without DB vars set
    
    * removes unused packages
    
    * Update tokens.css file to reflect Aon design foundations
    
    * Fix fon-family so that it is applied consistently. Body is inconsistent with shadow dom
    
    * Comment cleanup.
    
    * save work
    
    * merge xavier
    
    * fixes footer rendering issue
    
    * Save work to merge Elijah solution
    
    * Save work to merge Elijah solution
    
    * fixes stencil tsconfig issue
    
    * Part of MIL-139: Complete stencils for navbar, footer, link-card, expansion-card, faq-card
    
    * part of MIL 141: adds docs folder
    
    * removed db comments
    
    * stencil rebuild
    
    * Fix stencil config
    
    * updating packages
    
    * Routing pages for SPA
    
    * Header,Title,Control-Card,Subnav-card,Blue-card Components
    
    * Correct navbar in stencil layer
    
    * Correct navbar
    
    * Fix typos in stencil causing build errors
    
    * part of MIL-145: creates pooled Postgres client
    
    * closes MIL-145: successfully connects to Supabase
    
    * completed some of faqs.tsx
    
    * closes MIL-147: adds migrations and ensures DB schema
    
    * Save work before pulling from Xavier
    
    * closes MIL-149: seeds sample json into Supabase
    
    * part of MIL-151: graphql can talk to supabase
    
    * updates packages
    
    * Import styling correctly to React level.
    
    * adds more comments to sever folders
    
    * Demo prep
    
    * Update gitignore to include loader folder and components.d.ts
    
    * removes generated stencil folders
    
    ---------
    
    Co-authored-by: Annie Tadvick <annie.tadvick@gmail.com>
    Co-authored-by: milapaprika <mila.henson@protonmail.com>
    Co-authored-by: Xavier <xglover410@gmail.com>
    Co-authored-by: Amen Stoney <amenstoney@gmail.comgit config --global user.name Amen>
    Co-authored-by: Annie Tadvick <153228151+anddude@users.noreply.github.com>

--- client/src/components/sections/faqs.tsx
+++ client/src/components/sections/faqs.tsx
@@ -2,9 +2,15 @@
   TL;DR  --> A button that when clicked expands to FAQs in the form of separate dropdowns for each question 
       1. Each FAQ will have a button that will expand or shrink to show the answer to that question
 ================================ */
-import React from "react";
+// import React from "react";
+
+import { useState } from 'react';
+
 
 export default function Faqs() {
+
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <section>
       <faq-card question="Do you feel secure?" answer="Lorem ipsum dolor sit amet"/>
