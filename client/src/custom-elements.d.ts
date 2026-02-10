ce50e78582a19be8dd7356b64bd0d4ce880a0a63
Elijah Morris <morrisxelijah@gmail.com>
2026-02-10 10:57:37 -0500

    chore: stop tracking generated files

--- client/src/custom-elements.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    'aon-blue-card': any;
-    'aon-navbar': any;
-    'aon-footer': any;
-  }
-}
