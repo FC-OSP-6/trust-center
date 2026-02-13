a81c141e9d1fc35f54a9d6e2525558243c7ff718
Elijah Morris <morrisxelijah@gmail.com>
2026-02-13 13:08:50 -0500

    Merge branch 'main' into elijah: cleans up merge artifacts

--- stencil/src/components/faq/faq-card.tsx
+++ stencil/src/components/faq/faq-card.tsx
@@ -97,15 +97,11 @@ export class FaqCard {
           </button>
         </header>
 
-<<<<<<< HEAD
         {this.isExpanded && (
           <p class="faq-answer" id="faq-content">
             {answer}
           </p>
         )}
-=======
-        <p class="faq-answer">{answer}</p>
->>>>>>> main
       </div>
     );
   }
