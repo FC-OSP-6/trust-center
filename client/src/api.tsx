3c9ff290b40139eb98676beb3d88447f97cedf55
Annie Tadvick <annie.tadvick@gmail.com>
2026-02-19 11:22:53 -0500

    remove format test code

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -4,5 +4,3 @@
       - build api query that could request needed data to fill
       each of the sections depending on which page we're on
 ================================ */
-
-//testing husky/prettier set up
