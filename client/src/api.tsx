ae51039ad519efe4df5bf0bfe7c87625d89c8af4
Annie Tadvick <annie.tadvick@gmail.com>
2026-02-19 11:22:27 -0500

    format test

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -4,3 +4,5 @@
       - build api query that could request needed data to fill
       each of the sections depending on which page we're on
 ================================ */
+
+//testing husky/prettier set up
